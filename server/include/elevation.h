71b16b893aff6b10e82d42768d57fda7bb17bc4d
Lemaire Arthur <arthur.lemaire@epitech.eu>
2018-05-31 17:12:13 +0200

    Added elevation.c and .h to create and fill el_requirements, an inventory_t array containing inventory_t structures representing the ressource requirements of all elevations

---
+++ server/include/elevation.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2018
+** elevation.h
+** File description:
+** header
+*/
+
+#ifndef ELEVATION_H_
+# define ELEVATION_H_
+
+#include "inventory.h"
+
+extern const inventory_t el_requirements[8];
+
+#endif /* !ELEVATION_H_ */
---
+++ server/src/elevation.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2018
+** elevation.c
+** File description:
+** Elevation requirements tab init
+*/
+
+#include "elevation.h"
+
+const inventory_t el_requirements[8] = {
+	{1, 0, 0, 0, 0, 0, 0},
+	{1, 1, 1, 0, 0, 0, 0},
+	{2, 0, 1, 0, 2, 0, 0},
+	{1, 1, 2, 0, 1, 0, 0},
+	{1, 2, 1, 3, 0, 0, 0},
+	{1, 2, 3, 0, 1, 0, 0},
+	{2, 2, 2, 2, 2, 1, 0},
+	{0, 0, 0, 0, 0, 0, 0}
+};
