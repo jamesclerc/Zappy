1eb59a2a2d1da0dc65b6865c0a865af2f2ad00ff
Lemaire Arthur <arthur.lemaire@epitech.eu>
2018-06-03 00:02:55 +0200

    Added player_create function with player.c file

---
+++ server/src/player.c
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2018
+** player.c
+** File description:
+** player related functions
+*/
+
+#include "player.h"
+
+player_t *player_create(int fd)
+{
+	player_t *new = malloc(sizeof(player_t));
+
+	if (!new)
+		return (NULL);
+	memset(new, 0, sizeof(player_t));
+	new->fd = fd;
+	new->stream = fdopen(fd, "r+");
+	if (!new->stream) {
+		free(new);
+		close(fd);
+		return (NULL);
+	}
+	return (new);
+}
