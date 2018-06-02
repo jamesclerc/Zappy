3585dc0f6ed871dc079683aea4fd66a3f37ad5b5
Lemaire Arthur <arthur.lemaire@epitech.eu>
2018-06-02 23:26:10 +0200

    Refactiring de handler.h en command.h

--- server/include/command.h
+++ server/include/command.h
@@ -1,21 +1,21 @@
 /*
 ** EPITECH PROJECT, 2018
-** handler.h
+** command.h
 ** File description:
 ** command handlers header
 */
 
 #pragma once
 
-typedef struct handler_s {
+typedef struct command_s {
 	char *name;
 	int duration;
 	void (*handle)(void **data);
 	void (*respond)(void *data);
-} handler_t;
-
-typedef struct command_s {
-	time_t start_time;
-	handler_t *cmd;
-	void *data;
 } command_t;
+
+typedef struct action_s {
+	time_t start_time;
+	command_t *cmd;
+	void *data;
+} action_t;
