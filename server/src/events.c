55ccc4ff8acc52d4cd3308e6e6ff686048873b15
Lemaire Arthur <arthur.lemaire@epitech.eu>
2018-06-03 04:16:23 +0200

    static command handlers array filled

--- server/src/events.c
+++ server/src/events.c
@@ -10,6 +10,23 @@
 #include "game.h"
 #include "entity.h"
 #include "communication.h"
+#include "commands.h"
+
+static command_t commands[] = {
+	{"Forward", 7, NULL, NULL},
+	{"Right", 7, NULL, NULL},
+	{"Left", 7, NULL, NULL},
+	{"Look", 7, NULL, NULL},
+	{"Inventory", 1, NULL, NULL},
+	{"Broadcast", 7, NULL, NULL},
+	{"Connect_nbr", 0, NULL, NULL},
+	{"Fork", 42, NULL, NULL},
+	{"Eject", 7, NULL, NULL},
+	{"Take", 7, NULL, NULL},
+	{"Set", 7, NULL, NULL},
+	{"Incantation", 300, NULL, NULL},
+	{NULL, 0, NULL, NULL}
+};
 
 bool accept_client(game_t *game, struct epoll_event *ev, int epoll_fd)
 {
