65fc9d2fff1ddaed142a737e37cf88c067b36fe4
James Clerc <james.clerc@epitech.eu>
2018-06-13 20:34:25 +0200

    adding the .h that must be in the include on server part

---
+++ server/src/map/map.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2018
+** PSU_2017_zappy
+** File description:
+** map.h
+*/
+
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "space.h"
+
+void fill_map(map_t *map);
+void fill_phiras(map_t *map, int percent);
+void fill_thystame(map_t *map, int percent);
