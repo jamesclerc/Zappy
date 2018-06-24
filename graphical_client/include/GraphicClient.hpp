bc278144587f1a00f6f689429217c713502a8f81
Kevin Freire <kevin.freire@epitech.eu>
2018-06-24 06:48:26 +0200

    Dependences done

--- graphical_client/include/GraphicClient.hpp
+++ graphical_client/include/GraphicClient.hpp
@@ -11,28 +11,29 @@
 
 #include "Player.hpp"
 #include "Map.hpp"
-#include "Communication.hpp"
 #include "menu.hpp"
+#include "Communication.hpp"
 
 namespace gpc {
 	class GraphicClient {
 		public:
-			static GraphicClient *getInstance();
-			GraphicClient(const GraphicClient&) = delete;
-			GraphicClient& operator=(const GraphicClient&) = delete;
+			GraphicClient(sf::RenderWindow *window, gpc::Menu *menu);
 			~GraphicClient();
+			void constructMap(int x, int y);
+			void completeTiles(int x, int y, std::vector<int> ressources);
+			void setTimer(int timer);
 			void addPlayer(Player p);
 			void run();
 		private:
+			int _timer;
 			void runMenu();
 			void initConnection();
 			void mainLoop();
 			sf::RenderWindow *_window;
 			static GraphicClient *_instance;
 			std::vector<gpc::Player> _players;
-			GraphicClient(sf::RenderWindow *window, gpc::Menu *menu);
 			gpc::Menu *_menu;
-			gpc::Communication _com;
+			Communication _com;
 			gpc::Map *_map;
 	};
 }
